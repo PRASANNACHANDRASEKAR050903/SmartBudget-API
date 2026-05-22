6b96cda012f881e30240b9c7dcf81d9d1d906c15
Prasanna Chandrasekar <prasannachandrasekhar05@gmail.com>
2026-05-22 10:33:03 +0530

    Fixed MySQL connection

--- Program.cs
+++ Program.cs
@@ -1,34 +1,54 @@
 using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using SmartBudget.API.Data;
 using SmartBudget.API.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// ==========================================
 // Controllers
+// ==========================================
+
 builder.Services.AddControllers();
 
+
+// ==========================================
 // Swagger
+// ==========================================
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Connection String
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// FIXED MySQL Version
+// ==========================================
+// Database Connection
+// ==========================================
+
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection");
+
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 36));
 
-// Database
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, serverVersion)
 );
 
-// Repositories
+
+// ==========================================
+// Repository Registration
+// ==========================================
+
 builder.Services.AddScoped<ExpenseRepository>();
 builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<BudgetRepository>();
 builder.Services.AddScoped<AISuggestionRepository>();
 
+
+// ==========================================
 // CORS
+// ==========================================
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
@@ -41,22 +61,27 @@ builder.Services.AddCors(options =>
         });
 });
 
+
+// ==========================================
+// Build App
+// ==========================================
+
 var app = builder.Build();
 
-// Swagger
+
+// ==========================================
+// Middleware
+// ==========================================
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// CORS
 app.UseCors("AllowFrontend");
 
-// HTTPS
 app.UseHttpsRedirection();
 
-// Authorization
 app.UseAuthorization();
 
-// Controllers
 app.MapControllers();
 
 app.Run();
